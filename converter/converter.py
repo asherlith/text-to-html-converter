3aac914d649109a3e160a172b12625cec0936e72
Asher <mahdis.ghadamyari@gmail.com>
2023-03-09 14:35:35 +0330

    now there is an HTML renderer instead of JSON, and some problems in regexes are fixed

--- converter/converter.py
+++ converter/converter.py
@@ -13,10 +13,9 @@ def find_max_tab(text):
 
 def nested_list(text):
     max_no = find_max_tab(text)
-    print(max_no)
     for i in range(1, max_no + 1):
         print(rf'((?<=\n\t{{{i}}})<li>[.\s\S]*?(?=\n\t{{0,{i - 1}}}<li>|\n<p>))')
-        text = re.sub(rf'((?<=\n\t{{{i}}})<li>[.\s\S]*?(?=\n\t{{0,{i - 1}}}<li>|\n<p>))', r'<ul>\1</ul>', text)
+        text = re.sub(rf'((?<=\n\t{{{i}}})<li>[.\s\S]*?(?=\n\t{{0,{i - 1}}}<li>|\n\t{{0,{i - 1}}}<p>|<\/ul>$))', r'<ul>\1</ul>', text)
     return text
 
 
@@ -34,14 +33,12 @@ def convert(text):
                    "®": "&reg;",
                    }
     new_text = text.translate(str.maketrans(conversions))
-
-    new_text = re.sub(r'(\u2022[.\s\S]*?(?=\n\t*[a-zA-Z0-9]|$))', r'<ul>\1</ul>', new_text)
     new_text = re.sub(r'((?<=\u2022).*)', r'<li>\1</li>', new_text)
+    new_text = re.sub(r'(\u2022[.\s\S]*?(?=\n\t*[a-zA-Z0-9]|$))', r'<ul>\1</ul>', new_text)
     new_text = new_text.replace("\n", "<br>\n")
 
     new_text = re.sub(r'([^</p>].*(?=<br>\n<br>)|[^>]+$)', r'<p>\1</p>', new_text)
     new_text = new_text.replace("•", "")
-
     new_text = nested_list(new_text)
-    print(new_text)
+    new_text = new_text.replace("<p>\n</p>", "")
     return new_text
--- converter/views.py
+++ converter/views.py
@@ -5,12 +5,17 @@ import json
 from rest_framework.response import Response
 from rest_framework import status
 from .converter import convert
+from rest_framework import renderers
 
 
 class TextConverter(GenericAPIView):
     serializer_class = TextSerializer
+    renderer_classes = [renderers.StaticHTMLRenderer]
 
     def post(self, request):
         text = json.loads(request.body.decode('utf-8'), strict=False)["text"]
         converted_text = convert(text)
-        return Response(data={"html": converted_text}, status=status.HTTP_200_OK)
+        return Response(converted_text, status=status.HTTP_200_OK)
+
+    def get(self, request):
+        return Response("Enter text to receive HTML")
